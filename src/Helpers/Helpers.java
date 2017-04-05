99f3b923057502bbe2a97dc210e60e375267f894
Cory Itzen <cditzen@iastate.edu>
2017-04-05 14:39:43 -0500

    Removed unused functions

--- src/Helpers/Helpers.java
+++ src/Helpers/Helpers.java
@@ -12,19 +12,6 @@ import java.util.List;
 public class Helpers {
 
     /**
-     * Returns true if the target is in the range x and y, non-inclusive
-     * @param x
-     * @param y
-     * @param target
-     * @return
-     */
-    public static boolean targetInRange(int x, int y, int target) {
-        int max = Math.max(x, y);
-        int min = Math.min(x, y);
-        return target < max && target > min;
-    }
-
-    /**
      * Returns true if a list of vertices are listed in anti clockwise order around a polygon
      * Credit: http://stackoverflow.com/questions/1165647/how-to-determine-if-a-list-of-polygon-points-are-in-clockwise-order
      * @param
@@ -41,72 +28,6 @@ public class Helpers {
 
         sum += leftSide * rightSide;
         return sum < 0;
-    }
-
-//    /**
-//     * Sorts a list of edges based on Edge Weight
-//     * All edges must implement IWeight
-//     * @param vertices list of edges to sort
-//     * @param
-//     */
-//    public static void mergeSort(List<Vertex> vertices) {
-//        mergeSort(vertices, 0, vertices.size() - 1);
-//    }
-
-    /**
-     * Divides and conquer a list of edges
-     * @param vertices list of edges to sort
-     * @param left left index
-     * @param right right index
-     * @param
-     */
-    private static void mergeSort(List<Vertex> vertices, int left, int right) {
-        if (left < right) {
-            int mid = left + (right - left) / 2;
-            mergeSort(vertices, left, mid);
-            mergeSort(vertices, mid + 1, right);
-            merge(vertices, left, mid, right);
-        }
-    }
-
-    /**
-     * Merges two array subsets into one ordered subset
-     * @param vertices List of edges to sort
-     * @param left left index of the first subset
-     * @param mid dividing index between two subsets
-     * @param right right index of the second subset
-     * @param
-     */
-    private static void merge(List<Vertex> vertices, int left, int mid, int right) {
-        /** Make a copy of the list of edges */
-        Vertex[] copy = new Vertex[vertices.size()];
-        for (int i = left; i <= right; i++) {
-            copy[i] = vertices.get(i);
-        }
-
-        /** Indices to iterate over */
-        int leftIndex = left;
-        int rightIndex = mid + 1;
-        int index = left;
-
-        /** Merge two array subsets based in increasing weight */
-        while (leftIndex <= mid && rightIndex <= right) {
-//            if (copy[leftIndex].compareTo(copy[rightIndex]) > 0) {
-//                vertices.set(index, copy[leftIndex]);
-//                leftIndex++;
-//            } else {
-//                vertices.set(index, copy[rightIndex]);
-//                rightIndex++;
-//            }
-            index++;
-        }
-
-        /** Fill in remaining edges */
-        while (leftIndex <= mid) {
-            vertices.set(index, copy[leftIndex]);
-            index++;
-            leftIndex++;
-        }
     }
 
     public static boolean vertexAboveNeighbors(Vertex target, Vertex n1, Vertex n2) {
